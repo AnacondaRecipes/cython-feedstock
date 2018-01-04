f4ebea0f67ad07934144abf61492830cedff31d4
Ray Donnelly <mingw.android@gmail.com>
2018-01-04 21:27:17 +0000

    Removal of LDFLAGS_CC on macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ ${HOST} =~ .*darwin.* ]]; then
-  export LDFLAGS=${LDFLAGS_CC}
-fi
-
 export CFLAGS="-I${PREFIX}/include -L${PREFIX}/lib ${CFLAGS}"
 ${PYTHON} setup.py install --single-version-externally-managed --record=record.txt
